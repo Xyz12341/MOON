356ca1518bfc08af8eadb393f03adca0b7b4f303
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-07-23 17:08:22 +0530

    Update welcome.py

--- VIPMUSIC/cplugin/welcome.py
+++ VIPMUSIC/cplugin/welcome.py
@@ -5,11 +5,11 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import ChatMemberUpdated, InlineKeyboardButton, InlineKeyboardMarkup
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/634e3ba7e32808c97ac88.jpg",
+    "https://telegra.ph/file/634e3ba7e32808c97ac88.jpg",
+    "https://telegra.ph/file/634e3ba7e32808c97ac88.jpg",
+    "https://telegra.ph/file/634e3ba7e32808c97ac88.jpg",
+    "https://telegra.ph/file/634e3ba7e32808c97ac88.jpg",
 ]
 # --------------------------------------------------------------------------------- #
 
