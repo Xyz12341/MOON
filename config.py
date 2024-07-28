fea3f4d7b15a137ab689f63aafc752eed214b975
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-07-28 15:47:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/Moonshining1/VIP-MUSIC-PLUGIN",
+    "https://github.com/Moonshining1/Vip-music-plugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
